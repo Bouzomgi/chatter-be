4dc9ca2522215f407586a2b93bbda907c17ceb4e
Brian Ouz <bouzomgi@gmail.com>
2024-05-27 20:34:52 -0400

    add openapi schema

--- src/routes/settingsRoutes.ts
+++ src/routes/settingsRoutes.ts
@@ -7,10 +7,39 @@ import { getDefaultAvatars } from '../storage/s3Accessors'
 
 const router = express.Router()
 
+router.get('/avatars', async (req: Request, res: Response) => {
+  try {
+    const authedReq = req as AuthedRequest
+
+    const defaultAvatars = await getDefaultAvatars()
+
+    const userProfile = await prisma.profile.findUnique({
+      where: {
+        userId: authedReq.userId
+      }
+    })
+
+    if (userProfile === null) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: 'Could not find user profile' })
+    }
+
+    return res.status(StatusCodes.OK).json({
+      defaultAvatars,
+      currentAvatar: userProfile.avatar
+    })
+  } catch {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: 'Could not change avatar' })
+  }
+})
+
 // TODO: make sure that the avatar is actually an avatar... not just like, random code
 // I think i can make a call to the BE, find all avatars, and store this so i dont keep making this req
 router.post(
-  '/avatar',
+  '/setAvatar',
   checkSchema({
     avatar: { in: ['body'], notEmpty: true }
   }),
@@ -45,34 +74,5 @@ router.post(
     }
   }
 )
-
-router.get('/avatars', async (req: Request, res: Response) => {
-  try {
-    const authedReq = req as AuthedRequest
-
-    const defaultAvatars = await getDefaultAvatars()
-
-    const userProfile = await prisma.profile.findUnique({
-      where: {
-        userId: authedReq.userId
-      }
-    })
-
-    if (userProfile === null) {
-      return res
-        .status(StatusCodes.NOT_FOUND)
-        .json({ error: 'Could not find user profile' })
-    }
-
-    return res.status(StatusCodes.OK).json({
-      defaultAvatars,
-      currentAvatar: userProfile.avatar
-    })
-  } catch {
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({ error: 'Could not change avatar' })
-  }
-})
 
 export default router
