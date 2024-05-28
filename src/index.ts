c3cd9de9e0b7f28cd9c23401ef82c7075dce7129
Brian Ouz <43935036+Bouzomgi@users.noreply.github.com>
2024-05-27 21:32:16 -0400

    Add api page (#12)
    * add swagger path
    
    * remove api stuff from tests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import app from './app'
 import env from './config'
+import swaggerDocs from './api/api-doc'
 
 // RUN THE SERVER
 app.listen(env.PORT, () => {
   console.log(`App listening on port ${env.PORT}`)
+
+  // Spins up OpenAPI docs route
+  swaggerDocs(app, '/docs')
+  console.info('Docs available at /docs')
 })
 
 export default app
