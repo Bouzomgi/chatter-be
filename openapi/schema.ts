32666254c1b87afdda6b3be421dc1d04d64ac99a
Brian Ouz <bouzomgi@gmail.com>
2024-06-12 00:34:56 -0400

    add yaml

--- openapi/schema.ts
+++ openapi/schema.ts
@@ -64,6 +64,7 @@ export interface paths {
             responses: {
                 201: components["responses"]["Created"];
                 400: components["responses"]["BadRequest"];
+                409: components["responses"]["Conflict"];
             };
         };
         delete?: never;
@@ -480,6 +481,18 @@ export interface components {
             content: {
                 "application/json": {
                     /** @example Resource not found. */
+                    error: string;
+                };
+            };
+        };
+        /** @description Conflict with current state of the resource */
+        Conflict: {
+            headers: {
+                [name: string]: unknown;
+            };
+            content: {
+                "application/json": {
+                    /** @example Conflict with current state of the resource. */
                     error: string;
                 };
             };
