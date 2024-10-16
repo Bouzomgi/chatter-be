67cd4f97b61a4e3c2147a5a11de5fb1c2221e409
Brian Ouz <bouzomgi@gmail.com>
2024-10-16 01:47:48 -0400

    w

--- scripts/write_env.sh
+++ scripts/write_env.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 # Create a file to hold environment variables
-ENV_FILE="/home/ec2-user/my-app/.env"
+ENV_FILE="/home/ec2-user/.env"
 
 # Create the directory if it doesn't exist
 mkdir -p "$(dirname "$ENV_FILE")"
 
 # Fetch the environment variables from SSM Parameter Store and write them to the .env file
-echo "PORT=$(aws ssm get-parameter --name /myapp/PORT --query 'Parameter.Value' --output text)" >> $ENV_FILE
-echo "DATABASE_URL=$(aws ssm get-parameter --name /myapp/DATABASE_URL --query 'Parameter.Value' --output text)" >> $ENV_FILE
-echo "STORAGE_BUCKET_NAME=$(aws ssm get-parameter --name /myapp/STORAGE_BUCKET_NAME --query 'Parameter.Value' --output text)" >> $ENV_FILE
-echo "AWS_S3_ENDPOINT=$(aws ssm get-parameter --name /myapp/AWS_S3_ENDPOINT --query 'Parameter.Value' --output text)" >> $ENV_FILE
-echo "AWS_DEFAULT_REGION=$(aws ssm get-parameter --name /myapp/AWS_DEFAULT_REGION --query 'Parameter.Value' --output text)" >> $ENV_FILE
-echo "TOKEN_SECRET=$(aws ssm get-parameter --name /myapp/TOKEN_SECRET --query 'Parameter.Value' --output text)" >> $ENV_FILE
-
-# Load the environment variables into the current session
-source $ENV_FILE
+echo "PORT='$(aws ssm get-parameter --name /myapp/PORT --query 'Parameter.Value' --output text)'" >> $ENV_FILE
+echo "DATABASE_URL='$(aws ssm get-parameter --name /myapp/DATABASE_URL --query 'Parameter.Value' --output text)'" >> $ENV_FILE
+echo "STORAGE_BUCKET_NAME='$(aws ssm get-parameter --name /myapp/STORAGE_BUCKET_NAME --query 'Parameter.Value' --output text)'" >> $ENV_FILE
+echo "AWS_S3_ENDPOINT='$(aws ssm get-parameter --name /myapp/AWS_S3_ENDPOINT --query 'Parameter.Value' --output text)'" >> $ENV_FILE
+echo "AWS_DEFAULT_REGION='$(aws ssm get-parameter --name /myapp/AWS_DEFAULT_REGION --query 'Parameter.Value' --output text)'" >> $ENV_FILE
+echo "TOKEN_SECRET='$(aws ssm get-parameter --with-decryption --name /myapp/TOKEN_SECRET --query 'Parameter.Value' --output
