8cfd0f286475895d6c5f087c21fd745df15fb192
Brian Ouz <bouzomgi@gmail.com>
2024-10-15 22:30:00 -0400

    a

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cd /home/ec2-user/my-app
+# Navigate to the application directory
+cd /home/ec2-user
+
+# Unzip the application package
+unzip my-app.zip
+
+# Remove the zip file if you no longer need it
+rm my-app.zip
+
+cd my-app
 
 npm run build
 node dist/src/index.js
