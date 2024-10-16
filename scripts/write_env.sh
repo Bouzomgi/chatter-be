ae4e0083cc7d15ddb196606b476112953e6a660a
Brian Ouz <bouzomgi@gmail.com>
2024-10-16 01:50:39 -0400

    w

--- scripts/write_env.sh
+++ scripts/write_env.sh
@@ -7,9 +7,12 @@ ENV_FILE="/home/ec2-user/.env"
 mkdir -p "$(dirname "$ENV_FILE")"
 
 # Fetch the environment variables from SSM Parameter Store and write them to the .env file
-echo "PORT='$(aws ssm get-parameter --name /myapp/PORT --query 'Parameter.Value' --output text)'" >> $ENV_FILE
-echo "DATABASE_URL='$(aws ssm get-parameter --name /myapp/DATABASE_URL --query 'Parameter.Value' --output text)'" >> $ENV_FILE
-echo "STORAGE_BUCKET_NAME='$(aws ssm get-parameter --name /myapp/STORAGE_BUCKET_NAME --query 'Parameter.Value' --output text)'" >> $ENV_FILE
-echo "AWS_S3_ENDPOINT='$(aws ssm get-parameter --name /myapp/AWS_S3_ENDPOINT --query 'Parameter.Value' --output text)'" >> $ENV_FILE
-echo "AWS_DEFAULT_REGION='$(aws ssm get-parameter --name /myapp/AWS_DEFAULT_REGION --query 'Parameter.Value' --output text)'" >> $ENV_FILE
-echo "TOKEN_SECRET='$(aws ssm get-parameter --with-decryption --name /myapp/TOKEN_SECRET --query 'Parameter.Value' --output
+echo "PORT=\"$(aws ssm get-parameter --name /myapp/PORT --query 'Parameter.Value' --output text)\"" >> $ENV_FILE
+echo "DATABASE_URL=\"$(aws ssm get-parameter --name /myapp/DATABASE_URL --query 'Parameter.Value' --output text)\"" >> $ENV_FILE
+echo "STORAGE_BUCKET_NAME=\"$(aws ssm get-parameter --name /myapp/STORAGE_BUCKET_NAME --query 'Parameter.Value' --output text)\"" >> $ENV_FILE
+echo "AWS_S3_ENDPOINT=\"$(aws ssm get-parameter --name /myapp/AWS_S3_ENDPOINT --query 'Parameter.Value' --output text)\"" >> $ENV_FILE
+echo "AWS_DEFAULT_REGION=\"$(aws ssm get-parameter --name /myapp/AWS_DEFAULT_REGION --query 'Parameter.Value' --output text)\"" >> $ENV_FILE
+echo "TOKEN_SECRET=\"$(aws ssm get-parameter --name /myapp/TOKEN_SECRET --with-decryption --query 'Parameter.Value' --output text)\"" >> $ENV_FILE
+
+# Load the environment variables into the current session
+source $ENV_FILE
