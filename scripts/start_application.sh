0c2fc72ca2a44c781dea6e781867d1b9b049ddb4
Brian Ouz <bouzomgi@gmail.com>
2024-10-16 00:55:34 -0400

    a

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # Navigate to the application directory
-cd /home/ec2-user
-
-# Unzip the application package
-unzip my-app
-
-cd my-app
+cd /home/ec2-user/my-app
 
 npm install --only=production
-source /etc/environment
+
 node dist/src/index.js
 
