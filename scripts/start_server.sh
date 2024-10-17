c6ffd96ebc1259cdcd3c8c839d551a6a9fe7a323
Brian Ouz <bouzomgi@gmail.com>
2024-10-16 23:03:16 -0400

    j

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Define the log file where the Node.js process output will be logged
-LOGFILE="/home/ec2-user/my-app/server.log"
-
 # Change to the directory where the application is located
 cd /home/ec2-user/my-app
 
 # Run the Node.js app in the background
-nohup node dist/src/index.js > $LOGFILE 2>&1 &
+nohup node dist/src/index.js &
 
 # Capture the process ID (PID) and store it in a file for later use
-echo $! > /home/ec2-user/my-app/nodeapp.pid
+echo $! > /home/ec2-user/nodeapp.pid
 
 # Disown the process so it's not attached to the terminal
 disown
 
-echo "Node.js server started in the background with PID $(cat /home/ec2-user/my-app/nodeapp.pid)"
+echo "Node.js server started in the background with PID $(cat /home/ec2-user/nodeapp.pid)"
