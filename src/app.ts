7f51ffc5e9868c5cfbf59929f4912c6361ecdee5
Brian Ouz <bouzomgi@gmail.com>
2025-01-28 22:39:24 -0500

    w

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import express from 'express'
 import http from 'http'
 import { StatusCodes } from 'http-status-codes'
 import swaggerDocs from './api/api-doc'
@@ -9,10 +8,17 @@ import { verifyToken } from './middlewares/tokenVerification'
 import authRoutes from './routes/authRoutes'
 import chatRoutes from './routes/chatRoutes'
 import settingsRoutes from './routes/settingsRoutes'
+import express, { Request, Response, NextFunction } from 'express'
 
 const app = express()
 
 // MIDDLEWARES
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+  console.log(`Incoming Request: ${fullUrl}`)
+  next() // Proceed to the next middleware (including CORS)
+})
+
 app.use(cors({ origin: env.FRONTEND_ENDPOINT, credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
