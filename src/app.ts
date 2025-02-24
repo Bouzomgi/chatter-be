d65da1cb946ffa5ce553a4ca37e8d3662100408d
Brian Ouz <bouzomgi@gmail.com>
2025-02-23 20:12:20 -0500

    w

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import http from 'http'
+import https from 'https'
 import { StatusCodes } from 'http-status-codes'
 import swaggerDocs from './api/api-doc'
 import env from './config'
@@ -8,17 +8,18 @@ import { verifyToken } from './middlewares/tokenVerification'
 import authRoutes from './routes/authRoutes'
 import chatRoutes from './routes/chatRoutes'
 import settingsRoutes from './routes/settingsRoutes'
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express'
+import fs from 'fs'
+import path from 'path'
 
 const app = express()
 
-// MIDDLEWARES
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
-  console.log(`Incoming Request: ${fullUrl}`)
-  next() // Proceed to the next middleware (including CORS)
-})
+const sslOptions = {
+  key: fs.readFileSync(path.join(__dirname, '../private/cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '../private/cert', 'cert.pem'))
+}
 
+// MIDDLEWARES
 app.use(cors({ origin: env.FRONTEND_ENDPOINT, credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
@@ -34,6 +35,6 @@ app.get('/api/health', (_, res) => {
   res.status(StatusCodes.OK).send('Up and running!')
 })
 
-const server = http.createServer(app)
+const server = https.createServer(sslOptions, app)
 
 export default server
