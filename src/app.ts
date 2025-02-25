b868e395ffe46c997483fadfbb06e49b0257ddf9
Brian Ouz <bouzomgi@gmail.com>
2025-02-25 01:36:06 -0500

    a

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import https from 'https'
+import http from 'http'
 import { StatusCodes } from 'http-status-codes'
 import swaggerDocs from './api/api-doc'
 import env from './config'
@@ -9,18 +9,13 @@ import authRoutes from './routes/authRoutes'
 import chatRoutes from './routes/chatRoutes'
 import settingsRoutes from './routes/settingsRoutes'
 import express from 'express'
-import fs from 'fs'
-import path from 'path'
 
 const app = express()
 
-const sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, '../private/cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '../private/cert', 'cert.pem'))
-}
-
 // MIDDLEWARES
 app.use(cors({ origin: env.FRONTEND_ENDPOINT, credentials: true }))
+app.use(cors({ origin: env.FRONTEND_ENDPOINT, credentials: true }))
+
 app.use(express.json())
 app.use(cookieParser())
 
@@ -32,9 +27,10 @@ swaggerDocs(app, 'api/docs')
 
 // ROUTES
 app.get('/api/health', (_, res) => {
-  res.status(StatusCodes.OK).send('Up and running!')
+  console.log('Health check request received')
+  res.status(StatusCodes.OK).send('Up and running!\n')
 })
 
-const server = https.createServer(sslOptions, app)
+const server = http.createServer(app)
 
 export default server
